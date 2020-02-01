6909eb299677835785f44ac5e0991f1dbce1c34b
gertjan <gertjan@mail.com>
2020-02-01 10:30:00 +0100

    2de poging

--- Lijsten/Lijsten.wpf/MainWindow.xaml.cs
+++ Lijsten/Lijsten.wpf/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace Lijsten.wpf
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstWeekdagen.Items.Add("maandag");
+            lstWeekdagen.Items.Add("dinsdag");
+            lstWeekdagen.Items.Add("woensdag");
+            lstWeekdagen.Items.Add("donderdag");
+            lstWeekdagen.Items.Add("vrijdag");
+            lstWeekdagen.Items.Add("zaterdag");
+            lstWeekdagen.Items.Add("zondag");
+
+            cmbWeekdagNummer.Items.Add(1);
+            cmbWeekdagNummer.Items.Add(2);
+            cmbWeekdagNummer.Items.Add(3);
+            cmbWeekdagNummer.Items.Add(4);
+            cmbWeekdagNummer.Items.Add(5);
+            cmbWeekdagNummer.Items.Add(6);
+            cmbWeekdagNummer.Items.Add(7);
+
+
+        }
+
+        private void lstWeekdagen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Title = "Je koos voor " + lstWeekdagen.SelectedItem.ToString();
+
+        }
+
+        private void cmbWeekdagNummer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(cmbWeekdagNummer.SelectedItem);
+            lstWeekdagen.SelectedIndex = (int)cmbWeekdagNummer.SelectedItem -1;
+        }
     }
 }
